8f796df002e4314cc33d490bc6ee02d01afef652
delkamaski <akamaski1@gmail.com>
2025-06-18 20:05:29 +0700

    tambah gallery view

--- fe/app/build.gradle.kts
+++ fe/app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+    implementation ("androidx.gridlayout:gridlayout:1.0.0")
 }
